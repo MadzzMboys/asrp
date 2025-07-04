440be97dbb29cad2c2606803d88592293112c24a
Madz <as5767181@gmail.com>
2025-07-04 16:12:41 +0700

    Add files via upload

--- asrp.js
+++ asrp.js
@@ -1,4 +1,3 @@
-
 function dc() {
   alert('Gabung & Bermain Di Server Air Space Roleplay')
   window.open("https://discord.gg/3HbyC3QAFS")
@@ -12,4 +11,10 @@ function yt() {
 function tt() {
   alert('Silahkan Follow Tiktok Founder ASRP')
   window.open("https://tiktok.com/@madzexcode")
+}
+
+function webf() {
+  if(confirm("Lanjut ke Website Official Founder ASRP")){
+    window.location.href="FOUNDER";
+  }
 }
